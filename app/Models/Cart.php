2bc3d6f533a69ae49e6dbc630b7b12a1a831f77b
Elvan Hidayat <elvanhd@gmail.com>
2023-07-31 21:54:42 +0700

    cart model relations

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
