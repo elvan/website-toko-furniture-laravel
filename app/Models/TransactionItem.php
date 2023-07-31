d6a012a12239a66ecda7dc0a2e13252c54a368c4
Elvan Hidayat <elvanhd@gmail.com>
2023-07-31 21:58:00 +0700

    transaction-item model relations

--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransactionItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'transaction_id',
+    ];
+
+    public function product()
+    {
+        return $this->hasOne(Product::class);
+    }
 }
