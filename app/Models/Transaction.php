5f5e73738234cca3583ffbe687b7dfbe567d96ab
Elvan Hidayat <elvanhd@gmail.com>
2023-07-31 21:56:23 +0700

    transaction model relations

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use App\Models\TransactionItem;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'address',
+        'phone',
+        'courier',
+        'payment',
+        'payment_url',
+        'total_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactionItems()
+    {
+        return $this->hasMany(TransactionItem::class);
+    }
 }
